d3da4c99d21fb228ba22522164f002f197badb50
Jan Kroulík <jk@wo.cz>
2026-06-15 20:56:17 +0200

    feat: export transports + REGISTERS + pymodbus dependency

--- pyatrea/__init__.py
+++ pyatrea/__init__.py
@@ -3,11 +3,18 @@ from .commands import CommandBuilder
 from .const import AtreaMode, AtreaProgram
 from .exceptions import (
     AtreaError, AtreaConnectionError, AtreaAuthError, AtreaResponseError,
+    AtreaModbusError,
 )
+from .http_transport import HttpTransport
+from .modbus_transport import ModbusTransport
 from .models import AtreaStatus, AtreaParams
+from .registers import REGISTERS, RegisterDef
+from .transport import AtreaTransport, Descriptors
 
 __all__ = [
     "AtreaClient", "CommandBuilder", "AtreaMode", "AtreaProgram",
     "AtreaStatus", "AtreaParams", "AtreaError", "AtreaConnectionError",
-    "AtreaAuthError", "AtreaResponseError",
+    "AtreaAuthError", "AtreaResponseError", "AtreaModbusError",
+    "AtreaTransport", "Descriptors", "HttpTransport", "ModbusTransport",
+    "REGISTERS", "RegisterDef",
 ]
